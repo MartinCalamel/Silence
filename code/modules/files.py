dacc28a1354183c36ab4cc23bf2da0e75907eef0
WallabyMt59 <wallabymt5.9@gmail.com>
2025-07-18 12:57:40 +0200

    add file fonction and error display

---
+++ code/modules/files.py
@@ -0,0 +1,79 @@
+"""
+# Auteur : Wallaby
+# Date : 2025-07-18
+# Objet : 
+# TODO : test unitaires
+"""
+
+import info
+import csv
+
+def exist_file(file_name:str)->bool:
+    """
+    # exist_file
+    ## Fonction
+    retourne si un fichoier existe ou non
+    ## Input
+    * file_name : str -> Nom du fichier + path si requis
+    ## Output
+    Bool
+    """
+    try:
+        f=open(file_name)
+        f.close()
+        return True
+    except FileNotFoundError:
+        info.error("Fichier non trouvé")
+        return False
+
+def read(file_name:str, delimiter:str)->list:
+    """
+    # read
+    ## Fonction
+    lit un fichier s'il exite et retourne son contenu sous la forme [lignes[delimiteur]]
+    ## Input
+    * file_name : str -> Nom du fichier + path si requis
+    * delimiter : str -> Symbole qui sépare les éléments d'une même ligne
+    ## Outputresult = file.read()
+    liste des lignes et des éléments
+    """
+    data:list = []
+    if exist_file(file_name):
+        with open(file_name, "r") as file:
+            content = csv.reader(file, delimiter= delimiter)
+            for ligne in content:
+                data.append(ligne)
+    return data
+
+def write(file_name:str, data:str)-> None:
+    """
+    # write
+    ## Fonction
+    ecrit le contenue de data dans le fichier en EFFACANT le contenue du fichier
+    ## Input
+    * file_name : str -> Nom du fichier + path si requis
+    * data : str -> Contenue à ecrire dans le fichier
+    ## Output
+    None
+    """
+    with open(file_name, "w") as file:
+        file.write(data)
+    return None
+
+def add(file_name:str, data:str)->None:
+    """
+    # add
+    ## Fonction
+    Ajoute le contenue de data au fichier s'il exite  
+    on créé une nouvelle ligne
+    ## Input
+    * file_name : str -> Nom du fichier + path si requis
+    * data : str -> Contenue à ajouter dans le fichier
+    ## Output
+    None
+    """
+    if exist_file(file_name):
+        with open(file_name, "a") as file:
+            file.write("\n")
+            file.write(data)
+    return None
---
+++ code/modules/info.py
@@ -0,0 +1,21 @@
+"""
+# Auteur : Wallaby
+# Date : 2025-07-18
+# Objet : 
+# TODO : 
+"""
+from colorama import Fore, init
+
+init()
+
+def error(message:str)-> None:
+    """
+    # error
+    ## Fonction
+    Affiche un message d'erreur en rouge
+    ## Input
+    * message : str -> message à afficher
+    ## Output
+    None
+    """
+    print(Fore.RED,"\nError : ",message,Fore.WHITE)
