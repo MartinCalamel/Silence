2059d99cbef6848d782ab5a4c1675a3c13fb0865
WallabyMt59 <wallabymt5.9@gmail.com>
2025-07-19 15:00:00 +0200

    change format

--- modules/files.py
+++ modules/files.py
@@ -1,14 +1,15 @@
 """
 # Auteur : Wallaby
 # Date : 2025-07-18
-# Objet : 
+# Objet :
 # TODO : test unitaires
 """
 
 import modules.info as info
 import csv
 
-def exist_file(file_name:str)->bool:
+
+def exist_file(file_name: str) -> bool:
     """
     # exist_file
     ## Fonction
@@ -19,37 +20,41 @@ def exist_file(file_name:str)->bool:
     Bool
     """
     try:
-        f=open(file_name)
+        f = open(file_name)
         f.close()
         return True
     except FileNotFoundError:
         info.error("Fichier non trouvé")
         return False
 
-def read(file_name:str, delimiter:str)->list:
+
+def read(file_name: str, delimiter: str) -> list: 
     """
     # read
     ## Fonction
-    lit un fichier s'il exite et retourne son contenu sous la forme [lignes[delimiteur]]
+    lit un fichier s'il exite et retourne son contenu sous 
+    la forme [lignes[delimiteur]]
     ## Input
     * file_name : str -> Nom du fichier + path si requis
     * delimiter : str -> Symbole qui sépare les éléments d'une même ligne
     ## Outputresult = file.read()
     liste des lignes et des éléments
     """
-    data:list = []
+    data: list = []
     if exist_file(file_name):
         with open(file_name, "r") as file:
-            content = csv.reader(file, delimiter= delimiter)
+            content = csv.reader(file, delimiter)
             for ligne in content:
                 data.append(ligne)
     return data
 
-def write(file_name:str, data:str)-> None:
+
+def write(file_name: str, data: str) -> None:
     """
     # write
     ## Fonction
-    ecrit le contenue de data dans le fichier en EFFACANT le contenue du fichier
+    ecrit le contenue de data dans le fichier en EFFACANT 
+    le contenue du fichier
     ## Input
     * file_name : str -> Nom du fichier + path si requis
     * data : str -> Contenue à ecrire dans le fichier
@@ -60,7 +65,8 @@ def write(file_name:str, data:str)-> None:
         file.write(data)
     return None
 
-def add(file_name:str, data:str)->None:
+
+def add(file_name: str, data: str) -> None:
     """
     # add
     ## Fonction
