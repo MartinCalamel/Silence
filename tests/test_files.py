604fcbbd21b92e52c0d62e1894e51463e12d8173
WallabyMt59 <wallabymt5.9@gmail.com>
2025-07-19 15:07:06 +0200

    correction test

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,10 +1,10 @@
 import pytest
 from modules.files import read, write, add, exist_file
 
+
 def test_files():
     file = "tests/test_file.txt"
-    assert exist_file(file) == True
-    assert exist_file("non_exist.txt") == False
+    assert exist_file(file)
+    assert not exist_file("non_exist.txt")
 
-    assert read(file,";") == [["data", "test"]]
-    
+    assert read(file, ";") == [["data", "test"]]
