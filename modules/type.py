1863eb5e9d4af29dff57c3a21f2c8a06e2d93152
WallabyMt59 <wallabymt5.9@gmail.com>
2025-09-09 17:53:21 +0200

    add type control fonction

---
+++ modules/type.py
@@ -0,0 +1,28 @@
+"""
+# Auteur : Wallaby
+# Date : 2025-09-09
+# Objet : gestion des type et verrification du bon format de ceux ci
+# TODO :    fonction de verification
+            gestion des erreurs
+"""
+
+from modules.info import error
+import sys
+
+
+def check_type(var: any, type_cible: any) -> bool:
+    """
+    # check_type
+    ## Fonction
+    Verifie que le type de la variable est bien celui attendu.  
+    Dans le cas contraire, génère une erreur.
+    ## Input
+    * var: any -> variable dont on veux verifier le type
+    * type_cible: any -> type dont doit être la variable
+    ## Output
+    * result: bool -> Resultat de la verification
+    """
+    if type(var) == type_cible:
+        return True
+    error("le type de la variable n'est pas le bon")
+    sys.exit()
