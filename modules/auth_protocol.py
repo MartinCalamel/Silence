8d73a6182a94e3a48201d3244be0e0f170916424
WallabyMt59 <wallabymt5.9@gmail.com>
2025-09-10 20:31:04 +0200

    ajout de fonction

--- modules/auth_protocol.py
+++ modules/auth_protocol.py
@@ -4,6 +4,7 @@
 # Objet : fonction pour le protocole d'authentification
 # TODO :    fonction d'enregistrement d'user
             Fonction de verrification d'utilisateur
+            couverture de test
 """
 
 import modules.files as files
@@ -13,6 +14,30 @@ import modules.info as info
 
 # Nom du fichier où sont enregistré les mots de passes et les nom d'utilisateur 
 PASSWORD_USERNAME_FILENAME: str = "/temp/passwd.txt"
+
+
+def is_username_used(username: str) -> bool:
+    """
+    # is username_used
+    ## Fonction
+    On verrifie dans le fichier des mots de passes
+    et noms d'utilisateur que le nom d'utilisateur n'y est pas.
+    ## Input
+    * username: str -> Nom d'utilisateur à controler
+    ## Output
+    * bool -> est ce que l'utilisateur est dans le fichier
+    """
+
+    # Verification des types (test des paramètres)
+    type.check_type(username, str)
+
+    data: list = files.read(PASSWORD_USERNAME_FILENAME, ";")
+    list_username: list = [ligne[0] for ligne in data]
+
+    if username in list_username:
+        info.error("Nom d'utilisateur déjà utilisé")
+        return False
+    return True
 
 
 def new_user(username: str, password: str) -> bool:
@@ -34,11 +59,7 @@ def new_user(username: str, password: str) -> bool:
     if not files.exist_file(PASSWORD_USERNAME_FILENAME):
         files.create_file(PASSWORD_USERNAME_FILENAME, "")
     else :
-        data: list = files.read(PASSWORD_USERNAME_FILENAME, ";")
-        list_username: list = [ligne[0] for ligne in data]
-        
-        if username in list_username:
-            info.error("Nom d'utilisateur déjà utilisé")
+        if is_username_used(username):
             return False
     
     hashed_password: str = hash.make_hash(password)
